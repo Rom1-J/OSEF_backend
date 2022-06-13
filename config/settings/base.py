f2bf76f7dacc9bf5a077c8126227ef9359125165
Romain J <romain.ordi@gmail.com>
2022-06-13 11:22:39 +0200

    feat(core): add admin panel template

--- config/settings/base.py
+++ config/settings/base.py
@@ -62,6 +62,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # APPS
 # -----------------------------------------------------------------------------
 DJANGO_APPS = [
+    "jazzmin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
