f314c6716eb7971756e0783087d43c6d1739113a
Romain J <romain.ordi@gmail.com>
2022-06-13 14:13:24 +0200

    feat(routes): add frontend as submodule

--- config/urls.py
+++ config/urls.py
@@ -1,28 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
+# noinspection PyTypeChecker
 urlpatterns = [
-    path(
-        "", TemplateView.as_view(template_name="pages/home.html"), name="home"
-    ),
-    path(
-        "about/",
-        TemplateView.as_view(template_name="pages/about.html"),
-        name="about",
-    ),
-    # Django Admin, use {% url 'admin:index' %}
-    path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path("users/", include("osef.apps.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  # Django Admin, use {% url 'admin:index' %}
+                  path(settings.ADMIN_URL, admin.site.urls),
+                  # Your stuff: custom urls includes go here
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
 urlpatterns += [
@@ -63,5 +53,9 @@ if settings.DEBUG:
         import debug_toolbar
 
         urlpatterns = [
-            path("__debug__/", include(debug_toolbar.urls))
-        ] + urlpatterns
+                          path("__debug__/", include(debug_toolbar.urls))
+                      ] + urlpatterns
+
+urlpatterns += [
+    path("", include("osef.apps.vue.urls", namespace="vue")),
+]
---
+++ osef/apps/vue/views.py
@@ -0,0 +1,60 @@
+from django.conf import settings
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+from django.views.generic import View
+
+
+class VueCSSView(View):
+    template_name = "dist/css/"
+
+    def get(self, request: WSGIRequest, path=""):
+        file_path = settings.ROOT_DIR / "osef/templates" / self.template_name
+
+        local_path = file_path / path
+
+        if local_path.exists() and local_path.is_file():
+            return HttpResponse(
+                local_path.read_text(),
+                content_type="text/css"
+            )
+
+        return HttpResponse(status=404)
+
+
+class VueJSView(View):
+    template_name = "dist/js/"
+
+    def get(self, request: WSGIRequest, path=""):
+        file_path = settings.ROOT_DIR / "osef/templates" / self.template_name
+
+        local_path = file_path / path
+
+        if local_path.exists() and local_path.is_file():
+            return HttpResponse(
+                local_path.read_text(),
+                content_type="application/javascript"
+            )
+
+        return HttpResponse(status=404)
+
+
+class VueImgView(View):
+    template_name = "dist/img/"
+
+    def get(self, request: WSGIRequest, path=""):
+        file_path = settings.ROOT_DIR / "osef/templates" / self.template_name
+
+        local_path = file_path / path
+        ext = local_path.split('.')[-1]
+
+        if local_path.exists() and local_path.is_file():
+            if ext in ["svg", "png", "jpg", "jpeg"]:
+                content_type = f"image/{ext}"
+
+                return HttpResponse(
+                    local_path.read_bytes(),
+                    content_type=content_type
+                )
+
+        return HttpResponse(status=404)
+
