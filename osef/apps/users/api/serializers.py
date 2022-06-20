9b1c8c2c4bd85a99c29c1a420be095d63705fd93
Romain J <romain.ordi@gmail.com>
2022-06-20 11:40:33 +0200

    feat(users:api): add user update routes

--- osef/apps/users/api/serializers.py
+++ osef/apps/users/api/serializers.py
@@ -7,7 +7,15 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "first_name", "url"]
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "avatar",
+            "friend_code",
+            "pub_key",
+            "url",
+        ]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "id"}
