fa9e97e62eab8220cbdc5f2b92d8d42ea758c187
Thomas KÖCK <thomas.kock@efrei.net>
2022-06-29 19:15:54 +0200

    Modif CSRF cookie HTTP only

--- config/settings/base.py
+++ config/settings/base.py
@@ -229,7 +229,8 @@ FIXTURE_DIRS = (str(APPS_DIR / "fixtures"),)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-httponly
 SESSION_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
-CSRF_COOKIE_HTTPONLY = True
+# Désactivé pour éviter les problème avec VueJs
+# CSRF_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
