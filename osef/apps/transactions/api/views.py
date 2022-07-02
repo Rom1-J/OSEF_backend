4b0391d280e1a96084ab96ee472eab848ed84bdd
Thomas KÖCK <thomas.kock@efrei.net>
2022-07-02 18:31:49 +0200

    style

--- osef/apps/transactions/api/views.py
+++ osef/apps/transactions/api/views.py
@@ -54,7 +54,8 @@ class TransactionsViewSet(
                 message=_(
                     "Hi %s, please add me to your OSEF network "
                     "<a href='%s'>%s</a>"
-                ) % (user1.username, accept_url, accept_url),
+                )
+                % (user1.username, accept_url, accept_url),
                 from_email="no-reply@osef.net",
                 recipient_list=[user2.email],
             )
@@ -73,7 +74,11 @@ class TransactionsViewSet(
         ).count() > 0:
 
             return Response(
-                {"message": "Ce contact existe déjà" if accepted else "Une demande a déjà été envoyée"},
+                {
+                    "message": "Ce contact existe déjà"
+                    if accepted
+                    else "Une demande a déjà été envoyée"
+                },
                 status=status.HTTP_302_FOUND,
                 headers=self.get_success_headers(serializer.data),
             )
@@ -99,9 +104,9 @@ class TransactionsViewSet(
             instance.save()
             send_mail(
                 subject=_("%s has accepted to connect")
-                        % instance.user2.username,
+                % instance.user2.username,
                 message=_("Hi %s, %s has just accepted your connection!")
-                        % (instance.user1.username, instance.user2.username),
+                % (instance.user1.username, instance.user2.username),
                 from_email="no-reply@osef.net",
                 recipient_list=[instance.user1.email],
             )
@@ -165,7 +170,7 @@ class FilesViewSet(
             send_mail(
                 subject=_("%s sent you a file!") % file.owner,
                 message=_("Hi %s, you have received a new file!")
-                        % file.receiver,
+                % file.receiver,
                 from_email="no-reply@osef.net",
                 recipient_list=[file.receiver.email],
             )
